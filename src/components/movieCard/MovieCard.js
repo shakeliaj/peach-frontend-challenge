99bef7860352f181b810b55a96781baa8e204e0e
Shakelia Johnson <shakeliamjohnson@gmail.com>
2024-08-31 00:07:49 -0400

    added MovieCard component

--- src/components/movieCard/MovieCard.js
+++ src/components/movieCard/MovieCard.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 
-const MovieCard = () => {
-  return <div>movie card</div>
+import styling from './movieCard.module.scss'
+
+const MovieCard = ({
+  Poster: poster, // movie image url
+  Title: title, // movie title
+  Year: year, // year movie was created
+  imdbID: id,
+  handleCardClick,
+}) => {
+  const imageNotAvailable = useMemo(() => poster === 'N/A', [poster])
+
+  return (
+    <div
+      className={styling['movie-card']}
+      role='button'
+      onClick={handleCardClick}
+    >
+      {imageNotAvailable ? (
+        <div>{title}<br />(image not available)</div>
+      ) : (
+        <img alt={title} src={poster} />
+      )}
+    </div>
+  )
+}
+
+MovieCard.propTypes = {
+  poster: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.string,
+  id: PropTypes.string,
+  handleCardClick: PropTypes.func,
 }
 
 export default MovieCard
