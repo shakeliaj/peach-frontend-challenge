9db194e3e1ce03996e39d511035a626ef8dbdcbd
Shakelia Johnson <shakeliamjohnson@gmail.com>
2024-09-03 13:01:11 -0400

    added comments to SearchInput component

--- src/components/searchInput/SearchInput.js
+++ src/components/searchInput/SearchInput.js
@@ -8,6 +8,10 @@ import { setSearchTerm } from '../../redux/actions'
 
 import styling from './searchInput.module.scss'
 
+// SearchInput component
+// Displayed on home (/) page
+// and in Navigation component (which is displayed on /search and /favorites pages)
+
 const SearchInput = ({ defaultValue, className }) => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -16,6 +20,9 @@ const SearchInput = ({ defaultValue, className }) => {
 
   useEffect(() => {
     if (inputRef.current) {
+      // Listener function so that when user clicks 'Enter' while in the search input,
+      // if there is a search term value, then the value is saved to Redux state as the
+      // searchTerm and the user is then navigated to the /search page
       const listener = (event) => {
         if (event.key === 'Enter' && inputRef?.current?.value) {
           event.stopImmediatePropagation()
@@ -35,6 +42,7 @@ const SearchInput = ({ defaultValue, className }) => {
     className && className,
   )
 
+  // Returns a container that has an input component for entering search text
   return (
     <div className={containerClasses}>
       <input
