577865ed0f429042362b5dc314b0b74e093d6db9
Shakelia Johnson <shakeliamjohnson@gmail.com>
2024-09-03 14:02:47 -0400

    removed logo; updated movie services (API calls)

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,25 +1,40 @@
 import axios from 'axios'
 
+const movieApiUrl = 'http://www.omdbapi.com/?apikey=d15d95d'
+
 const getMoviesBySearch = async ({ searchTerm = 'guardians', pageNum = 1 }) => {
   // Make a call to the OMDB API using the passed in searchTerm and pageNum values as params
   const response =
-    await axios.get(`http://www.omdbapi.com/?apikey=d15d95d&s=${searchTerm}&page=${pageNum}&type=movie`)
+    await axios.get(`${movieApiUrl}&s=${searchTerm}&page=${pageNum}&type=movie`)
 
   if (response?.data) {
-    const {
-      Search: searchDetails,
-      totalResults = 0,
-      Error: errorMessage,
-    } = response.data
-
-    // If a list of movies is returned, return the list and total number of movies
-    // Otherwise, if there is an error, return the error
-    if (searchDetails) {
-      return { movies: searchDetails, totalResults }
-    } else if (errorMessage) {
-      return { error: errorMessage }
+    // If there is an error, return the error
+    // Otherwise, if a list of movies is returned, return the list and total number of movies
+    if (response?.data?.Error) {
+      return { error: response.data.Error }
+    } else {
+      return {
+        movies: response?.data?.Search,
+        totalResults: response?.data?.totalResults,
+      }
     }
   }
 }
 
-export { getMoviesBySearch }
+
+const getMovieById = async (id = '') => {
+  // Make a call to the OMDB API using the passed in id as params
+  const response = await axios.get(`${movieApiUrl}&i=${id}&plot=full`)
+
+  if (response?.data) {
+    // If there is an error, return the error
+    // Otherwise, if a movie is returned, return the movie information
+    if (response?.data?.Error) {
+      return { error: response.data.Error }
+    } else {
+      return { details: response?.data }
+    }
+  }
+}
+
+export { getMoviesBySearch, getMovieById }
